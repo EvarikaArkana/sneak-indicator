f34a8bd7592ce9f82f67688666ca1f7cb4f161dd
Roundaround <roundaroundna@gmail.com>
2025-06-21 13:29:37 -0400

    Update to 1.21.6

--- src/main/java/me/roundaround/sprintindicator/mixin/InGameHudMixin.java
+++ src/main/java/me/roundaround/sprintindicator/mixin/InGameHudMixin.java
@@ -1,18 +1,14 @@
 package me.roundaround.sprintindicator.mixin;
 
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.client.texture.Sprite;
-import net.minecraft.client.texture.StatusEffectSpriteManager;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Arm;
-import net.minecraft.util.Colors;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -24,10 +20,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
-  @Final
-  @Shadow
-  private MinecraftClient client;
-
   @Shadow
   private PlayerEntity getCameraPlayer() {
     return null;
@@ -35,7 +27,7 @@ public abstract class InGameHudMixin {
 
   @Inject(method = "renderHotbar", at = @At(value = "TAIL"))
   private void afterRenderHotbar(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-    PlayerEntity basePlayer = getCameraPlayer();
+    PlayerEntity basePlayer = this.getCameraPlayer();
     if (!(basePlayer instanceof ClientPlayerEntity player)) {
       return;
     }
@@ -51,15 +43,14 @@ public abstract class InGameHudMixin {
     int x = arm == Arm.LEFT ? (scaledWidth + 182) / 2 + 6 : (scaledWidth - 182) / 2 - 18 - 6;
     int y = scaledHeight - 20;
 
-    StatusEffectSpriteManager statusEffectSpriteManager = this.client.getStatusEffectSpriteManager();
-    Sprite sprite = statusEffectSpriteManager.getSprite(StatusEffects.SPEED);
-    context.drawSpriteStretched(RenderLayer::getGuiTextured, sprite, x, y, 18, 18, Colors.WHITE);
+    Identifier texture = InGameHud.getEffectTexture(StatusEffects.SPEED);
+    context.drawGuiTexture(RenderPipelines.GUI_TEXTURED, texture, x, y, 18, 18);
   }
 
   @ModifyArg(
       method = "renderHotbar", at = @At(
       value = "INVOKE",
-      target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;" +
+      target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;" +
                "Lnet/minecraft/util/Identifier;IIII)V"
   ), index = 2, slice = @Slice(
       from = @At(
@@ -78,7 +69,7 @@ public abstract class InGameHudMixin {
   @ModifyArg(
       method = "renderHotbar", at = @At(
       value = "INVOKE",
-      target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;" +
+      target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;" +
                "Lnet/minecraft/util/Identifier;IIIIIIII)V"
   ), index = 6, slice = @Slice(
       from = @At(
@@ -96,7 +87,7 @@ public abstract class InGameHudMixin {
 
   @Unique
   private int adjustAttackIndicatorX(int original) {
-    PlayerEntity player = getCameraPlayer();
+    PlayerEntity player = this.getCameraPlayer();
     if (player == null) {
       return original;
     }
